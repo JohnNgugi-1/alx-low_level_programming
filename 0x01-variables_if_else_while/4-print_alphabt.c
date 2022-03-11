0d014fd3c0ee4d915f7be2a4aa1c547363f80c25
Alx-john <john.ngugi6@yahoo.com>
2022-03-11 21:21:42 +0300

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Description: print alphabet in small letters except e and q
+ * Return: 0
+ */
+int main(void)
+{
+        char ch;
+
+        for (ch = 'a'; ch <= 'z'; ch++)
+		if (ch = 'e' && ch = 'q')
+			return;
+		else
+                	putchar(ch);
+        putchar('\n');
+        return (0);
+}
+
