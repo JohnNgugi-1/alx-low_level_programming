30c7b30bfa47023486629b46ae520ea63256a301
Alx-john <john.ngugi6@yahoo.com>
2022-03-11 17:20:35 +0300

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Description: print alphabet in capital letters
+ * Return: 0
+ */
+int main(void)
+{
+	for(char ch = 'A';ch<'Z';ch++)
+		putchar(ch);
+	return 0;
+}
+
