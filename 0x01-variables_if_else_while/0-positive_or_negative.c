fc4b3ecb47d74e8f6d4dc65083d3ea03f4fd9498
Alx-john <john.ngugi6@yahoo.com>
2022-03-11 16:01:56 +0300

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,6 +5,7 @@
 /* betty style doc for function main goes there */
 /*
  * main - function
+ * Description: positive_or_negative
  * return value : 0
  */
 int main(void)
@@ -16,11 +17,11 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-	printf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-        printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
         }
 	else
 	{
