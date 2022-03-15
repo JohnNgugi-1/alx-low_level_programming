b87f3ccca8e2571dcf8aee6d91ef8001feac1eb7
Alx-john <john.ngugi6@yahoo.com>
2022-03-15 16:25:44 +0300

    lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Description: print alphabe in lowercase
+ * Return: 0
+*/
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+	return (0);
+}
