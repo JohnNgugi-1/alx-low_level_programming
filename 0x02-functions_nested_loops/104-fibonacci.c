2637ea91390bcd970eae639cd864233c142886f6
Alx-john <john.ngugi6@yahoo.com>
2022-03-15 23:28:51 +0300

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,16 +8,16 @@
 int main(void)
 {
 	int i = 0;
-	unsigned long int a = 0, b = 1, next = 0;
+	long int a = 0, b = 1, next = 0;
 
-	while (i < 96)
+	while (i < 98)
 	{
 		next = a + b;
 		a = b;
 		b = next;
 		printf("%lu", next);
 
-		if (i < 96)
+		if (i < 97)
 			printf(", ");
 		i++;
 	}
