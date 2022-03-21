25d8690258a3ce13ece09d28c53286fe78f04e8b
Alx-john <john.ngugi6@yahoo.com>
2022-03-21 16:25:09 +0300

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - function
+ * @a : first parameter
+ * @b : second parameter
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int i = a;
+
+	a = b;
+	b = i;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,2 +1,3 @@
 char _putchar(char i);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
