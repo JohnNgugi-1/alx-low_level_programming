d19f1ef8bca155b05fb317e576436db0b165f4f3
Alx-john <john.ngugi6@yahoo.com>
2022-03-24 15:29:37 +0300

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcat - function
+ * @dest : first string
+ * @src : second string
+ * Return: @dest + @src
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *con;
+
+	_putchar("%s%s", dest, src);
+	con = &dest
+
+	return (con);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+char *_strcat(char *dest, char *src);
