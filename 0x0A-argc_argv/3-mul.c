0c16ce1065f89306b88e4c47aeee40c014a54eee
Alx-john <john.ngugi6@yahoo.com>
2022-04-01 15:35:40 +0300

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function
+ * @argc: contains argument count
+ * @argv: contains argument values
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int result;
+
+	if (argc = 3)
+	{
+		i = argv[1];
+		j = argv[2];
+		int result = i * j;
+		printf("%d\n", result);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+
+	return (1);
+}
