35bcd0b4e11bfc6c9dcdc9c268af72cb963b207e
Alx-john <john.ngugi6@yahoo.com>
2022-04-01 14:20:56 +0300

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * argc : contains argument count
+ * argv : contains argument values
+ * Return : integer to be returned
+ */
+
+int main(int argc, char* argv[])
+{
+	print("%s\n", argv[0]);
+}
