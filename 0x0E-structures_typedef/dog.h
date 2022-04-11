14cbd84d1ab53b15bef5fff16bdc0bb8a35c25bd
Alx-john <john.ngugi6@yahoo.com>
2022-04-11 21:22:25 +0300

    dog header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
 struct dog
 {
 	char *name;
@@ -7,5 +17,8 @@ struct dog
 	char *owner;
 }
 
+/**
+ * dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
 #endif
